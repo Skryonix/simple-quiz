475d1a27252ed617313b4c058abfb457482f1d00
Skryonix <143288032+Skryonix@users.noreply.github.com>
2023-10-21 11:22:32 -0500

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -1,32 +1,72 @@
 let currentQuestionIndex = 0;
 const questions = [
     {
-        question: 'What is the capital of France?',
-        answers: ['London', 'Berlin', 'Paris', 'Rome'],
+        question: 'Who is the author of "The Lord of the Rings"?',
+        answers: ['J.K. Rowling', 'George R.R. Martin', 'J.R.R. Tolkien', 'Stephen King'],
         correct: 2
-    }
-    // Add more questions as needed
+    },
+    {
+        question: 'What is the name of the hobbit who inherits the One Ring?',
+        answers: ['Frodo Baggins', 'Samwise Gamgee', 'Meriadoc Brandybuck', 'Peregrin Took'],
+        correct: 0
+    },
+  {
+    question: 'What is the name of the wizard who leads the Fellowship of the Ring?',
+    answers: ['Saruman', 'Gandalf', 'Radagast', 'Alatar'],
+    correct: 1
+},
+{
+    question: 'Which creature from the Misty Mountains tries to eat the ring-bearer?',
+    answers: ['Balrog', 'Smaug', 'Gollum', 'Shelob'],
+    correct: 2
+},
+{
+    question: 'What is the elven word for friend, used to open the gates of Moria?',
+    answers: ['Mellon', 'Elbereth', 'Elessar', 'Nenya'],
+    correct: 0
+},
+{
+    question: 'Who is the king of Rohan?',
+    answers: ['Éomer', 'Théoden', 'Denethor', 'Faramir'],
+    correct: 1
+},
+{
+    question: 'Who defeats the Witch-King of Angmar in the Battle of the Pelennor Fields?',
+    answers: ['Aragorn', 'Éowyn', 'Legolas', 'Gimli'],
+    correct: 1
+},
+{
+    question: 'Which member of the Fellowship is lost in the Mines of Moria?',
+    answers: ['Boromir', 'Gimli', 'Gandalf', 'Legolas'],
+    correct: 2
+},
+    // ... Add more questions here
 ];
 
 function showQuestion(question) {
-    $('#question').text(question.question);
-    $('#answer-buttons').empty();
-    question.answers.forEach((answer, index) => {
-        const button = $('<button></button>').text(answer).addClass('btn');
-        button.on('click', () => selectAnswer(index));
-        $('#answer-buttons').append(button);
-    });
+    $('#question-container').css({opacity: 0, transform: 'translateY(-20px)'});  // Reset animation
+    setTimeout(() => {  // Delay to allow animation reset
+        $('#question').text(question.question);
+        $('#answer-buttons').empty();
+        question.answers.forEach((answer, index) => {
+            const button = $('<button></button>').text(answer).addClass('btn');
+            button.on('click', () => selectAnswer(index));
+            $('#answer-buttons').append(button);
+        });
+        $('#question-container').css({opacity: 1, transform: 'translateY(0)'});  // Trigger animation
+    }, 100);
 }
 
 function selectAnswer(index) {
-    const correct = questions[currentQuestionIndex].correct === index;
+    const correctIndex = questions[currentQuestionIndex].correct;
+    const correct = correctIndex === index;
+    $('#answer-buttons .btn').eq(correctIndex).addClass('correct-answer');
     if(correct) {
-        alert('Correct!');
     } else {
-        alert('Incorrect!');
     }
     $('#next-button').show();
 }
+
 
 function next() {
     currentQuestionIndex++;
@@ -35,9 +75,15 @@ function next() {
         $('#next-button').hide();
     } else {
         // End of quiz
-        alert('Quiz completed');
+        const restart = confirm('Quiz completed. Would you like to restart?');
+        if (restart) {
+            currentQuestionIndex = 0;
+            showQuestion(questions[currentQuestionIndex]);
+            $('#next-button').hide();
+        }
     }
 }
+
 
 showQuestion(questions[currentQuestionIndex]);
 
